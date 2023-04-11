c47b725d161c663a3ed065fe50f5a492d6270d0e
lunky84 <john.downie@thekeysupport.com>
2023-04-11 14:45:54 +0100

    utilise useScroll to move camera

--- src/routes/scroll-controls.tsx
+++ src/routes/scroll-controls.tsx
@@ -1,9 +1,10 @@
-import { Canvas } from "@react-three/fiber";
-import { ScrollControls, Scroll } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import { ScrollControls, Scroll, useScroll } from "@react-three/drei";
 
 import { useControls, folder } from "leva";
 
-function ScrollControlsExample() {
+function ScrollControlsExample(): JSX.Element {
   const { damping } = useControls({
     "Scroll options": folder({
       damping: {
@@ -15,27 +16,58 @@ function ScrollControlsExample() {
     }),
   });
 
+  function MyScene() {
+    const scroll = useScroll();
+
+    useFrame((state, delta) => {
+      // The offset is between 0 and 1, you can apply it to your models any way you like
+      console.log(scroll.offset);
+      const offset = 1 - scroll.offset;
+      state.camera.position.set(
+        Math.sin(offset) * -10,
+        Math.atan(offset * Math.PI * 2) * 5,
+        Math.cos((offset * Math.PI) / 3) * -10
+      );
+      state.camera.lookAt(0, 0, 0);
+    });
+
+    return (
+      <>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+        <mesh position={[0, -4, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+        <mesh position={[0, -8, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+        <Scroll html>
+          <section className="section">
+            <h2>My Portfolio</h2>
+          </section>
+          <section className="section">
+            <h2>My projects</h2>
+          </section>
+          <section className="section">
+            <h2>Contact me</h2>
+          </section>
+        </Scroll>
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <Canvas
-          style={{ backgroundColor: "#f4a1f1" }}
-          camera={{ position: [0, -1, 5] }}
-        >
+      <div className="webgl">
+        <Canvas camera={{ position: [0, 0, 10] }} gl={{ alpha: true }}>
           <axesHelper args={[5]} />
-
-          <ScrollControls pages={3} damping={damping}>
-            {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
-
-            <Scroll>{/* Canvas contents in here will scroll along */}</Scroll>
-            <Scroll html>
-              {/* DOM contents in here will scroll along */}
-              <h1>html in here (optional)</h1>
-              <h1 style={{ top: "100vh", position: "absolute" }}>
-                second page
-              </h1>
-              <h1 style={{ top: "200vh", position: "absolute" }}>third page</h1>
-            </Scroll>
+          <ambientLight intensity={0.5} />
+          <ScrollControls pages={3}>
+            <MyScene />
           </ScrollControls>
         </Canvas>
       </div>
