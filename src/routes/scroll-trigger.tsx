df0908d17bd33463e0aa662f485b96368a16d37d
lunky84 <john.downie@thekeysupport.com>
2023-04-15 21:13:13 +0100

    add mouse follower hover state

--- src/routes/scroll-trigger.tsx
+++ src/routes/scroll-trigger.tsx
@@ -35,14 +35,32 @@ function ScrollTriggerExample(): JSX.Element {
       goToSection(hash);
     }
 
-    gsap.set("#ball", { xPercent: -50, yPercent: -50 });
+    const ball = document.getElementById("ball");
 
-    let xTo = gsap.quickTo("#ball", "x", { duration: 0.6, ease: "power3" }),
-      yTo = gsap.quickTo("#ball", "y", { duration: 0.6, ease: "power3" });
+    gsap.set(ball, { xPercent: -50, yPercent: -50 });
+
+    let xTo = gsap.quickTo(ball, "x", { duration: 0.3, ease: "power3" }),
+      yTo = gsap.quickTo(ball, "y", { duration: 0.3, ease: "power3" });
 
     window.addEventListener("mousemove", (e) => {
       xTo(e.clientX);
       yTo(e.clientY);
+    });
+
+    const matches = document.querySelectorAll("a, .button");
+
+    matches.forEach((element) => {
+      element.addEventListener("mouseover", () => {
+        if (ball) {
+          ball.classList.add("hover");
+        }
+      });
+
+      element.addEventListener("mouseout", () => {
+        if (ball) {
+          ball.classList.remove("hover");
+        }
+      });
     });
 
     const observer = new IntersectionObserver(
