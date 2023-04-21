a48830f311da0f04722c145417d8931d2414046c
lunky84 <john.downie@thekeysupport.com>
2023-04-21 14:36:38 +0100

    animated svg page intro

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,17 +1,39 @@
 import { useProgress } from "@react-three/drei";
 import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ReactSVG } from "react-svg";
 
 export const LoadingScreen = ({ started, onStarted }) => {
   const { progress } = useProgress();
 
   useEffect(() => {
     if (progress === 100) {
-      onStarted();
+      var tl = gsap.timeline({ onComplete: () => onStarted() });
+      tl.to(".loadingScreen__logo", {
+        scale: 1.2,
+        duration: 0.3,
+        ease: "power2.inOut",
+        delay: 0.8,
+      }).to(".loadingScreen__logo", {
+        scale: 0,
+        duration: 0.5,
+        ease: "power2.in",
+      });
+
+      gsap.to("#mask", {
+        height: 100 - progress,
+        onComplete: () => {
+          tl.play();
+        },
+      });
     }
   }, [progress]);
 
   return (
     <div className={`loadingScreen ${started ? "loadingScreen--started" : ""}`}>
+      <div className="loadingScreen__logo">
+        <ReactSVG src="./Threejs-logo.svg" />
+      </div>
       <div className="loadingScreen__progress">
         <div
           className="loadingScreen__progress__value"
